6b6358f20d9b12b3aeda4c8ca3174617f1c9ce58
skomet84-code <skomet84@gmail.com>
2026-09-14 13:30:49 +0900

    chore: sync shared version to v1.0.2

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,5 @@
 export const GAME_TITLE = 'JUNJA WORLD';
-export const GAME_VERSION = '1.0.1';
+export const GAME_VERSION = '1.0.2';
 export const WORLD_WIDTH = 2400;
 export const WORLD_HEIGHT = 1350;
 export const PLAYER_SPEED = 230;
