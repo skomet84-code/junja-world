7044662120fe9c0aa1cb88502fbf294ec580ac54
skomet84-code <skomet84@gmail.com>
2026-09-14 17:29:53 +0900

    chore: bump game version to 2.7.2

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,5 @@
 export const GAME_TITLE = 'JUNJA WORLD';
-export const GAME_VERSION = '2.7.1';
+export const GAME_VERSION = '2.7.2';
 export const WORLD_WIDTH = 2400;
 export const WORLD_HEIGHT = 1350;
 export const PLAYER_SPEED = 230;
